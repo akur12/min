b73e4810c82e4160b986d8e6fe476331ad7111c4
akur12 <34025524+akur12@users.noreply.github.com>
2017-11-27 17:56:04 +0700

    Update fftauto.js

--- fftauto.js
+++ fftauto.js
@@ -12,11 +12,11 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'lezapbriantiy',
+    password: 'adi.kau1'
 }
 
-const TargetUsername = '';
+const TargetUsername = 'lexapebriatys';
 
 const Login = async function(User){
 
